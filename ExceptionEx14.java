06aaa2b215fd86565fcd40a59386d719b44aec1f
원종찬 <100653363+brownshit@users.noreply.github.com>
2022-08-12 14:15:33 +0900

    Add files via upload

---
+++ ExceptionEx14.java
@@ -0,0 +1,13 @@
+public class ExceptionEx14 {
+    public static void main(String[] args) {
+        try {
+            method1();
+        } catch (Exception e){
+            System.out.println("method1메서드에서 예외가 처리되었습니다.");
+            e.printStackTrace();
+        }
+    }
+    static void method1() throws Exception{
+        throw new Exception();
+    }//method1 end
+}
